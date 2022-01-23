e26618a3f3ee3d15cf5acbfaaf8b743c7bf7f427
gdlyeabkov <gdlyeabkov@gmail.com>
2022-01-23 11:23:39 +0300

    add_select

--- SpriteEditor/MainWindow.xaml.cs
+++ SpriteEditor/MainWindow.xaml.cs
@@ -23,9 +23,19 @@ namespace SpriteEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public Brush spriteBorderColor;
+        public Brush spriteLabelBackgroundColor;
+        public Brush emptyColor;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            spriteBorderColor = System.Windows.Media.Brushes.Green;
+            spriteLabelBackgroundColor = System.Windows.Media.Brushes.LightSkyBlue;
+            emptyColor = System.Windows.Media.Brushes.Transparent;
+
         }
 
         private void CreateSpriteSheetHandler(object sender, RoutedEventArgs e)
@@ -38,7 +48,12 @@ namespace SpriteEditor
                 Stream[] myStreams;
                 if ((myStreams = ofd.OpenFiles()) != null)
                 {
-                    DrawSpriteSheet();
+                    ClearSpriteSheet();
+                    DrawSpriteSheet(ofd);
+                    ClearSpritesLabels();
+                    DrawSpritesLabels(ofd);
+                    bool isHaveSprites = myStreams.Length >= 1;
+                    ToggleSaveSpriteSheetBtn(isHaveSprites);
                 }
 
             }
@@ -66,20 +81,25 @@ namespace SpriteEditor
             }
         }
 
-        public void DrawSpriteSheet()
+        public void DrawSpriteSheet(OpenFileDialog ofd)
         {
             float spriteXCoord = 0;
             float spriteYCoord = 0;
+            List<float> spriteHeights = new List<float>();
             foreach (string fileName in ofd.FileNames)
             {
                 Rectangle sprite = new Rectangle();
+                sprite.DataContext = fileName.ToString();
+                sprite.MouseUp += SelectSpriteHandler;
                 System.Drawing.Image uploadedImage = System.Drawing.Image.FromFile(fileName);
                 float spriteWidth = uploadedImage.Width;
                 float spriteHeight = uploadedImage.Height;
+                spriteHeights.Add(spriteHeight); 
                 bool isWrapSprites = canvas.ActualWidth < spriteXCoord + spriteWidth;
                 if (isWrapSprites)
                 {
-                    spriteYCoord += spriteHeight;
+                    float maxSpriteHeight = spriteHeights.Max();
+                    spriteYCoord += maxSpriteHeight;
                     spriteXCoord = 0;
                 }
                 sprite.Width = spriteWidth;
@@ -95,7 +115,196 @@ namespace SpriteEditor
                 sprite.Fill = spriteBrush;
                 canvas.Children.Add(sprite);
                 spriteXCoord += spriteWidth;
+                bool isOutlineEnabled = ((bool)(isDisplayOutline.IsChecked));
+                Brush currentSpriteStroke = null;
+                if (isOutlineEnabled)
+                {
+                    currentSpriteStroke = spriteBorderColor;
+                }
+                else
+                {
+                    currentSpriteStroke = emptyColor;
+                }
+                sprite.Stroke = currentSpriteStroke;
             }
+        }
+
+        public void ClearSpriteSheet()
+        {
+            canvas.Children.Clear();
+        }
+
+        public void SelectSpriteHandler(object sender, RoutedEventArgs e)
+        {
+            Rectangle sprite = ((Rectangle)(sender));
+            string spriteName = sprite.DataContext.ToString();
+            ClearSpritesSelection();
+            SelectSprite(sprite);
+            SelectSpriteLabel(spriteName);
+        }
+
+        public void ClearSpritesSelection()
+        {
+            UIElementCollection sprites = canvas.Children;
+            foreach (Rectangle sprite in sprites)
+            {
+                ImageBrush spriteBrush = new ImageBrush();
+                BitmapImage spriteSource = new BitmapImage();
+                spriteSource.BeginInit();
+                string fileName = sprite.DataContext.ToString();
+                spriteSource.UriSource = new Uri(fileName, UriKind.Absolute);
+                spriteSource.EndInit();
+                spriteBrush.ImageSource = spriteSource;
+                sprite.Fill = spriteBrush;
+            }
+        }
+
+        public void DrawSpritesLabels(OpenFileDialog ofd)
+        {
+            foreach (string fileName in ofd.FileNames)
+            {
+                StackPanel spriteLabel = new StackPanel();
+                spriteLabel.Orientation = Orientation.Horizontal;
+                spriteLabel.DataContext = fileName;
+                spriteLabel.MouseUp += SelectSpriteLabelHandler;
+                Image spriteLabelIcon = new Image();
+                BitmapImage spriteSource = new BitmapImage();
+                spriteSource.BeginInit();
+                spriteSource.UriSource = new Uri(fileName, UriKind.Absolute);
+                spriteSource.EndInit();
+                spriteLabelIcon.Source = spriteSource;
+                spriteLabelIcon.Width = 20;
+                spriteLabelIcon.Height = 20;
+                spriteLabelIcon.Margin = new Thickness(10);
+                spriteLabel.Children.Add(spriteLabelIcon);
+                TextBlock spriteLabelName = new TextBlock();
+                string onlyFileName = GetTerminatedName(fileName);
+                spriteLabelName.Text = onlyFileName;
+                spriteLabelName.Margin = new Thickness(10);
+                spriteLabel.Children.Add(spriteLabelName);
+                spritesLabels.Children.Add(spriteLabel);
+            }
+        }
+
+        public void ClearSpritesLabels()
+        {
+            spritesLabels.Children.Clear();
+        }
+
+        public void SelectSprite(Rectangle sprite)
+        {
+            VisualBrush newSpriteBrush = new VisualBrush();
+            Grid newSpriteBrushContent = new Grid();
+            ImageBrush newSpriteBrushContentInitial = ((ImageBrush)(sprite.Fill));
+            Image rawNewSpriteBrushContentInitial = new Image();
+            BitmapImage spriteSource = new BitmapImage();
+            spriteSource.BeginInit();
+            spriteSource.UriSource = new Uri(newSpriteBrushContentInitial.ImageSource.ToString(), UriKind.Absolute);
+            spriteSource.EndInit();
+            rawNewSpriteBrushContentInitial.Source = spriteSource;
+            newSpriteBrushContent.Children.Add(rawNewSpriteBrushContentInitial);
+            Rectangle newSpriteBrushContentSelected = new Rectangle();
+            SolidColorBrush newSpriteBrushContentSelectedFill = new SolidColorBrush();
+            Color newSpriteBrushContentSelectedFillColor = new Color();
+            newSpriteBrushContentSelectedFillColor.R = 0;
+            newSpriteBrushContentSelectedFillColor.G = 0;
+            newSpriteBrushContentSelectedFillColor.B = 255;
+            newSpriteBrushContentSelectedFillColor.A = 100;
+            newSpriteBrushContentSelectedFill.Color = newSpriteBrushContentSelectedFillColor;
+            newSpriteBrushContentSelected.Fill = newSpriteBrushContentSelectedFill;
+            newSpriteBrushContent.Children.Add(newSpriteBrushContentSelected);
+            newSpriteBrush.Visual = newSpriteBrushContent;
+            sprite.Fill = newSpriteBrush;
+        }
+
+        public void SelectSpriteLabel(string spriteName)
+        {
+            string onlySpriteName = GetTerminatedName(spriteName);
+            foreach (StackPanel spriteLabel in spritesLabels.Children)
+            {
+                TextBlock rawSpriteLabelName = ((TextBlock)(spriteLabel.Children[1]));
+                string spriteLabelName = rawSpriteLabelName.Text;
+                bool isSpriteLabelFound = spriteLabelName == onlySpriteName;
+                if (isSpriteLabelFound)
+                {
+                    ClearSpritesLabelsSelection();
+                    spriteLabel.Background = spriteLabelBackgroundColor;
+                }
+            }
+        }
+
+        public void ClearSpritesLabelsSelection()
+        {
+            UIElementCollection labels = spritesLabels.Children;
+            foreach (StackPanel spriteLabel in labels)
+            {
+                spriteLabel.Background = emptyColor;
+            }
+        }
+
+        public void SelectSpriteLabelHandler (object sender, RoutedEventArgs e)
+        {
+            StackPanel sprite = ((StackPanel)(sender));
+            string spriteName = sprite.DataContext.ToString();
+            ClearSpritesSelection();
+            Rectangle foundedSprite = null;
+            bool isSpriteFound = false;
+            foreach (Rectangle currentSprite in canvas.Children)
+            {
+                string currentSpriteName = currentSprite.DataContext.ToString();
+                bool isSpritesNamesMatch = currentSpriteName == spriteName;
+                if (isSpritesNamesMatch)
+                {
+                    isSpriteFound = true;
+                    foundedSprite = currentSprite;
+                }
+            }
+            if (isSpriteFound)
+            {
+                SelectSprite(foundedSprite);
+                SelectSpriteLabel(spriteName);
+            }
+        }
+
+        public string GetTerminatedName (string fileName)
+        {
+            string terminatedName = "";
+            string[] separatablePart = fileName.Split(new char[] { '\\', '/' });
+            int separatablePartLength = separatablePart.Length - 1;
+            terminatedName = separatablePart[separatablePartLength];
+            return terminatedName;
+        }
+
+        private void ToggleSpriteOutlineHandler(object sender, RoutedEventArgs e)
+        {
+            CheckBox checkBox = ((CheckBox)(sender));
+            bool isDisplayOutline = ((bool)(checkBox.IsChecked));
+            UIElementCollection sprites = canvas.Children;
+            bool isSpritesInserted = sprites.Count >= 1;
+            if (isSpritesInserted)
+            {
+                Rectangle firstSprite = ((Rectangle)(sprites[0]));
+                Brush spriteOutline = firstSprite.Stroke;
+                bool isOutlineEnabled = isDisplayOutline;
+                Brush currentSpriteStroke = null;
+                if (isOutlineEnabled)
+                {
+                    currentSpriteStroke = spriteBorderColor;
+                }
+                else
+                {
+                    currentSpriteStroke = emptyColor;
+                }
+                foreach (Rectangle sprite in sprites)
+                {
+                    sprite.Stroke = currentSpriteStroke;
+                }
+            }
+        }
+
+        public void ToggleSaveSpriteSheetBtn (bool toggler)
+        {
+            saveSpriteSheetBtn.IsEnabled = toggler;
         }
 
     }
